7aec2936f6232b102a49f5c24330853f8a194799
icukaegbu <icukaegbu@gmail.com>
2014-08-19 13:29:00 +0100

    Refactored Database Schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,16 +1,16 @@
-/*DROP DATABASE chat;*/
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 /* Create other tables and define schemas for them here! */
-CREATE TABLE Rooms(
-	id int not null auto_increment,
-	name varchar(20) not null,
+-- CREATE TABLE Rooms(
+-- 	id int not null auto_increment,
+-- 	name varchar(20) not null,
 
-	PRIMARY KEY(id),
-	UNIQUE(name)
-);
+-- 	PRIMARY KEY(id),
+-- 	UNIQUE(name)
+-- );
 
 CREATE TABLE Users(
 	id int not null auto_increment,
@@ -26,16 +26,26 @@ CREATE TABLE Messages(
 	id int not null auto_increment,
 	message varchar(255) not null,
 	user_id int not null,
-	room_id int,
+	roomname varchar(20),
 
 	PRIMARY KEY (id),
-	FOREIGN KEY (user_id) REFERENCES Users(id),
-	FOREIGN KEY (room_id) REFERENCES Rooms(id)
+	FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
+-- CREATE TABLE Messages(
+-- 	id int not null auto_increment,
+-- 	message varchar(255) not null,
+-- 	user_id int not null,
+-- 	room_id int,
 
-CREATE VIEW user_messages AS SELECT Users.username, Messages.message, Rooms.name FROM Users, Messages, Rooms
-WHERE Messages.user_id = Users.user_id AND Messages.room_id = Rooms.id;
+-- 	PRIMARY KEY (id),
+-- 	FOREIGN KEY (user_id) REFERENCES Users(id),
+-- 	FOREIGN KEY (room_id) REFERENCES Rooms(id)
+-- );
+
+
+CREATE VIEW user_messages AS SELECT Users.username, Messages.message, Messages.roomname FROM Users, Messages
+WHERE Messages.user_id = Users.id;
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
