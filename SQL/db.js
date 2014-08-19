2c84edd391802d5b08d1046c8b1a2fcfcf550195
icukaegbu <icukaegbu@gmail.com>
2014-08-19 17:07:51 +0100

    Edited db.js, wrote database queries

--- SQL/db.js
+++ SQL/db.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
+  user: "root",
   password: "",
   database: "chat"
 });
@@ -22,13 +22,41 @@ dbConnection.connect();
 
 
 exports.findAllMessages = function(cb){
+	//select all message
+	var querystring = 'SELECT * FROM user_messages';
+
+	dbConnection.query(querystring, [], function(err, rows, fields){
+		cb(err, rows);
+	});
+
+	//dbConnection.end();
 };
 
 exports.findUser = function(username, cb){
+	//find a specific user in the DB
+	var querystring = 'SELECT * FROM Users SET ?';
+
+	dbConnection.query(querystring, [username], function(err, rows, fields){
+		cb(err, rows);
+	});
+
+	//dbConnection.end();
 };
 
 exports.saveUser = function(username, cb){
+	var querystring = 'INSERT INTO Users SET ?';
+
+	dbConnection.query(querystring, [username], function(err, rows, fields){
+		cb(err, rows);
+	});
+
+	//dbConnection.end();
 };
 
 exports.saveMessage = function(message, userid, roomname, cb){
+	var querystring = 'INSERT INTO Messages SET ?';
+
+	dbConnection.query(query, [message, userid, roomname], function(err, rows, fields){
+		cb(err, rows);
+	});
 };
